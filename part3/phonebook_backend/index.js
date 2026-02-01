2507bb8c9c5c9fe8ca5d1eee9664848132766e2a
Peler Yuan <pelerlittlepig@gmail.com>
2026-02-01 20:33:50 +1000

    part3 deploy on render

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const {request} = require("express");
 const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
+app.use(cors)
 
 morgan.token('body', (request) => {
     return JSON.stringify(request.body)
@@ -95,7 +97,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
