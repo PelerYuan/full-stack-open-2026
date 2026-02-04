60ff8c9948756a2eeaaad19ec610957adbccdc5a
Peler Yuan <pelerlittlepig@gmail.com>
2026-02-05 05:13:05 +1000

    4.20 extra

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -16,10 +16,12 @@ mongoose.connect(config.MONGODB_URL)
 app.use(cors())
 app.use(express.json())
 
+app.use(middleware.tokenExtractor)
+app.use(middleware.errorHandler)
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-app.use(middleware.errorHandler)
 
 module.exports = app
